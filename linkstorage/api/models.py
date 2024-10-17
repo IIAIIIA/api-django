aa6eeef25764b4d34358f57ac49c60ca2585a320
Pavel Laman <mealaman@yandex.by>
2024-10-17 08:19:44 +0300

    Created a custom user model, link model, and collection model. Change the user model in the settings.

--- linkstorage/api/models.py
+++ linkstorage/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='collections')
+
+class Link(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True, null=True)
+    url = models.URLField(unique=True)
+    image = models.URLField(blank=True, null=True)
+    link_type = models.CharField(max_length=50, default='website')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='links')
+    collections = models.ManyToManyField(Collection, related_name='links')
