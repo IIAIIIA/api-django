230fc7c2a65af219c5327a5757cf583382697aa4
Pavel Laman <mealaman@yandex.by>
2024-10-17 09:50:43 +0300

    Added models to admin panel

--- linkstorage/api/admin.py
+++ linkstorage/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CustomUser, Link, Collection
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fields = ('email','password',('is_staff', 'is_active'), 'date_joined')
+    readonly_fields = ('date_joined',)
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Link)
+admin.site.register(Collection)
+
+
